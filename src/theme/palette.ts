d1d83fe2accee1e7dbaa83532fe4ea8902b9c6b3
Hat_Cloud <mr.hatcloud@gmail.com>
2023-07-06 16:17:11 +0800

    chore: 准备好兼具，配色，字体等相关文件

---
+++ src/theme/palette.ts
@@ -0,0 +1,6 @@
+const palette = {
+    Background: '#000000',
+    BackgroundGrey: '#1c1c1e',
+}
+
+export default palette
---
+++ src/utils/utils.ts
@@ -0,0 +1,12 @@
+import {Dice} from '../data/Character'
+
+/**
+ * 投掷 count 次 dice 面的骰子
+ */
+export const rollDice = (count: number, dice: Dice): number => {
+    let result = 0
+    for (let i = 0; i < count; i++) {
+        result += Math.floor(Math.random() * dice) + 1
+    }
+    return result
+}
